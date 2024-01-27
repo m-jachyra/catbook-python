73b8893ca4c5f3a1c6a758102499f9d3b901e82d
Kamil Stolarz <freeku132@gmail.com>
2024-01-27 23:18:36 +0100

    Fix storage path

--- backend/app/data/cat_image/cat_image_service.py
+++ backend/app/data/cat_image/cat_image_service.py
@@ -2,7 +2,7 @@ import uuid
 from fastapi import HTTPException, File
 from sqlalchemy.orm import Session
 from starlette import status
-
+from core.config import settings
 from data.base_service import BaseService
 from data.cat_image.cat_image import CatImageCreate, CatImageUpdate
 from data.storage_file.storage_file_service import storage_files_service
@@ -10,7 +10,7 @@ from db_context.models import CatImage
 
 
 class CatImagesService(BaseService[CatImage, CatImageCreate, CatImageUpdate]):
-    IMAGE_DIR = "data/storage_file/images/"
+    IMAGE_DIR = settings.IMAGES_DIR
 
     def create_profile_image(self, db: Session, image: File, cat_id: int):
         image.filename = f"{uuid.uuid4()}.jpg"
@@ -53,13 +53,14 @@ class CatImagesService(BaseService[CatImage, CatImageCreate, CatImageUpdate]):
         if existing_profile_image:
             try:
                 db.delete(existing_profile_image)
-
-                cat_db = CatImage(cat_id=cat_id, storage_file_id=storage_file.id, is_profile=True)
-                db.add(cat_db)
-                db.commit()
-                db.refresh(cat_db)
             except Exception as e:
                 raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Failed uploading image")
+
+        cat_db = CatImage(cat_id=cat_id, storage_file_id=storage_file.id, is_profile=True)
+        db.add(cat_db)
+        db.commit()
+        db.refresh(cat_db)
+
 
 
 cat_images_service = CatImagesService(CatImage)
