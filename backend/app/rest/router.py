dfb6d660169986dc457c4acbd2435d6eb6d3ae57
Marek Jachyra <marek792783@gmail.com>
2024-01-27 18:41:47 +0100

    Revert "Add temporary fixes"
    This reverts commit da7fc46436b4867b3160376c06c0f5e5d7ed9830.

--- backend/app/rest/router.py
+++ backend/app/rest/router.py
@@ -5,6 +5,6 @@ from .controllers import cats_controller, users_controller, default_controller, 
 router = APIRouter()
 
 router.include_router(default_controller.router, tags=["default"])
-# router.include_router(auth_controller.router, tags=["/auth"])
+router.include_router(auth_controller.router, tags=["/auth"])
 router.include_router(cats_controller.router, prefix="/cats", tags=["cats"])
 router.include_router(users_controller.router, prefix="/users", tags=["users"])
